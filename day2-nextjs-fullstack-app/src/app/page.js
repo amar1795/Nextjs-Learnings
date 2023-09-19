e76872ea0c1147e331b1c7787bdb72c1c793279f
Amar <jeetamar496@gmail.com>
2023-09-19 18:16:04 +0530

    hero page created and understood the concept of image in nextjs

--- day2-nextjs-fullstack-app/src/app/page.js
+++ day2-nextjs-fullstack-app/src/app/page.js
@@ -1,10 +1,23 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import hero from "public/hero.png"
 
 export default function Home() {
   return (
-    <>
-    <h1>this is a Homepage</h1>
-    </>
+    <div className={styles.container}>
+    <div className={styles.item}>
+      <h1 className={styles.title}>Better Design for your digital products</h1>
+      <p className={styles.desc}>Turning your ideas into reality. We bring togerther the teams from the global tech industry</p>
+      <button className={styles.button}>
+        See our Works
+      </button>
+    </div>
+
+    <div className={styles.item}>
+    <Image src={hero} className={styles.img}></Image>
+    </div>
+
+
+    </div>
   )
 }
--- day2-nextjs-fullstack-app/src/components/footer/Footer.jsx
+++ day2-nextjs-fullstack-app/src/components/footer/Footer.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
 
 
 const Footer = () => {
   return (
+    <>
     <div className={styles.container}>
      <div>
      @2023 BLOGTASTIC .ALL RIGHTS RESERVED.
      </div>
+
+      {/* can also use this and set the size of the image ,the size needs to be added when not importing */}
+      {/* <div className={styles.container}></div> */}
+     <div className={styles.social}>
+      <Image src="/1.png" width={25} height={25} className={styles.icon} alt="fb"></Image>
+      <Image src="/2.png" width={25} height={25} className={styles.icon} alt="insta"></Image>
+      <Image src="/3.png" width={25} height={25} className={styles.icon} alt="twitter"></Image>
+      <Image src="/4.png" width={25} height={25} className={styles.icon} alt="youtube"></Image>
+      
+     </div>
     </div>
+    </>
   )
 }
 
