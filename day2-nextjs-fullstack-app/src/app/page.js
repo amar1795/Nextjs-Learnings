e36cd5f375fbfb0f964a211ccaca1e0cec677899
Amar <jeetamar496@gmail.com>
2023-09-19 18:59:42 +0530

    button created as a component for its global use

--- day2-nextjs-fullstack-app/src/app/page.js
+++ day2-nextjs-fullstack-app/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import hero from "public/hero.png"
+import Button from '@/components/button/Button'
 
 export default function Home() {
   return (
@@ -8,9 +9,8 @@ export default function Home() {
     <div className={styles.item}>
       <h1 className={styles.title}>Better Design for your digital products</h1>
       <p className={styles.desc}>Turning your ideas into reality. We bring togerther the teams from the global tech industry</p>
-      <button className={styles.button}>
-        See our Works
-      </button>
+      
+      <Button text=" See our Works" url="/portfolio"/>
     </div>
 
     <div className={styles.item}>
