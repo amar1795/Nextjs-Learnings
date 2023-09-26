81213ea578f3096b03520bcfcbecb62415bc9c24
Amar <jeetamar496@gmail.com>
2023-09-27 00:08:38 +0530

    login error corrected

--- day2-nextjs-fullstack-app/src/app/api/auth/[...nextauth]/route.js
+++ day2-nextjs-fullstack-app/src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
+import connect  from "@/utils/db"
 import nextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-
+import User from "@/models/User"
+import bcrypt from "bcryptjs"
+import  CredentialsProvider  from "next-auth/providers/credentials"
 
 
 const handler = nextAuth({
@@ -10,8 +13,49 @@ const handler = nextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
+    CredentialsProvider({
+    id:"credentials",
+    name:"Credentials",
+    async authorize(credentials){
+        await connect();
+
+        try {
+            const user=await User.findOne({email:credentials.email,})
+
+            if(user)
+            {
+                const isPasswordCorrect= await bcrypt.compare(
+                    credentials.password,
+                    user.password
+                );
+
+                if(isPasswordCorrect)
+                {
+                    console.log("logged in successfully")
+                    return user;
+                }
+                else{
+                    throw new Error("wrong credentials")
+                }
+
+            }
+            else{
+                throw new Error("user not found")
+            }
+        } catch (error) {
+            throw new Error(error)          
+        }
+    }
+   })
   ],
+//   this will show the error in the same page in the params and not in a seperate page
+  pages:{
+    error:"/dashboard/login",
+  }
 })
+
+
+
+
 
 export {handler as GET,handler as POST}
--- day2-nextjs-fullstack-app/src/app/dashboard/(auth)/login/page.jsx
+++ day2-nextjs-fullstack-app/src/app/dashboard/(auth)/login/page.jsx
@@ -1,11 +1,30 @@
 "use client"
-import { signIn } from 'next-auth/react'
+import { getProviders,signIn,useSession } from 'next-auth/react'
 import React from 'react'
+import styles from "./page.module.css"
 
 const Login = () => {
+  const session=useSession();
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    const email=e.target[0].value;
+    const password=e.target[1].value;
+    signIn("credentials",{email,password,})
+  }
   return (
-    <div>
-      <button onClick={()=>signIn("google")}>Login with Google</button>
+    <div className={styles.container}>
+
+    <form className={styles.form} onSubmit={handleSubmit}>
+       
+        <input type='text' placeholder='email' required className={styles.input}  />
+        <input type='password' placeholder='password'  required className={styles.input}  />
+      <button className={styles.button}>Login with Credentials</button>
+      </form>
+      <button className={styles.google} onClick={()=>signIn("google")}>Login with Google</button>
+
+
+      
     </div>
   )
 }
